18d49af8b8217bd2150c4bc9abb71fab0e63e5f0
Lauren Wenzel <Laurenzel93@aol.com>
2021-06-22 20:40:49 -0400

    added all pieces of details page into components

---
+++ client/src/components/details/InspectionInfo/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import 'style.css';
+
+function InspectionInfo() {
+    return (
+        <div className="container-fluid border rounded border-primary p-4 justify-content-center" id="inspectionInfoContainer">
+            <div className="col justify-content-md-center">
+                <table className="table table-bordered table-primary">
+                    <tbody>
+                        <tr>
+                            <td id="date">Date</td>
+                            <td id="address">Address</td>
+                            <td id="inspectionType">Inspection Type</td>
+                            <td id="permitNumber">Permit Number</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" id="notes">Notes: </td>
+                            <td colspan="2" id="timestamp">Timestamp</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    )
+}
+
+export default InspectionInfo;
