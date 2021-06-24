2900eb4b4ad161146f5e543623b972bedf98a3ee
Jakob Knicley <knicleyjakob@gmail.com>
2021-06-24 19:30:55 -0400

    changes to loginform stuff

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -7,49 +7,61 @@ import './style.css';
     
 function LoginForm(props) {
 
-    const useFormInput = (initialValue) =>{
-        const [value, setValue] = useState(initialValue) 
-    }
-
     const [loading, setLoading] = useState(false);
     const username = useFormInput('');
     const password = useFormInput('');
     const [error, setError] = useState(null);
-
-    
    
     const loginHandler = () => {
+        console.log(username, password)
         setError(null);
         setLoading(true);
-        login( { username: username.value, password: password.value }).then(res => {
-            setLoading(false);
-            //setUserSession(res.data.token, res.data.user);
-            props.history.push('/dashboard')
-        })
+        login( { username: username.value, password: password.value })
+            .then(res => {
+                setLoading(false);
+                //setUserSession(res.data.token, res.data.user);
+                props.history.push('/dashboard')
+            })
+            .catch(error => {
+                setLoading(false);
+                if(error.response.status === 401) setError(error.response.data.message);
+                else setError("Something went wrong. Please try again later");
+            })
     }
 
     return (
         <main>
             <div className="loginFormContainer">
-                <form className='loginForm'>
+                <form className='loginForm' onSubmit={loginHandler}>
                     <div id="loginHeader" className="form-text">
                         Please enter your account information
                     </div>
                     <div className="mb-3">
                         <label htmlFor="username" className="form-label">Username</label>
-                        <input type="text" className="form-control" id="username" aria-describedby="usernameEntry" />
+                        <input type="text" {...username}className="form-control" id="username" aria-describedby="usernameEntry" />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password" />
+                        <input type="password" {...password}className="form-control" id="password" />
                     </div>
                     <div className="btnContainer">
-                        <button type="submit" className="btn customBtn" id="submitBtn" onClick={loginHandler}>Submit</button>
+                        <button type="submit" className="btn customBtn" id="submitBtn">Submit</button>
                     </div>
                 </form>
             </div>
         </main>
     )
+}
+
+const useFormInput = initialValue => {
+    const [value, setValue] = useState(initialValue);
+    const handleChange = e => {
+        setValue(e.target.value);
+    }
+    return {
+        value,
+        onChange: handleChange
+    }
 }
 
 export default LoginForm;
