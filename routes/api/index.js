801d9ff75c8354132c8fb51d019eb9754c59c3f3
Jakob Knicley <knicleyjakob@gmail.com>
2021-06-29 21:03:38 -0400

    was in the wrong directory ...oops. Real results api changes here

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const inspectionRoutes = require("./inspections.js");
 const userRoutes = require("./users");
 const permitRoutes = require("./permits");
+const resultRoutes = require("./results")
 
 router.use("/inspections", inspectionRoutes);
 router.use("/users", userRoutes);
 router.use("/permits", permitRoutes);
+router.use("/results", resultRoutes);
 
 module.exports = router;
