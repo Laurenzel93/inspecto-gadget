cbaf9763ba490d0f92e49d77ab305b7f4228ac49
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-06-30 22:35:35 -0400

    Dashboard formating take-26.

--- client/src/components/dashboard/UpcomingInspections/index.js
+++ client/src/components/dashboard/UpcomingInspections/index.js
@@ -2,22 +2,22 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 
-export function DateCard({children}) {
+export function DateCard({ children }) {
     return (
-        <div className="card border border-dark m-1">
-          <table className="card-body table table-striped">
-            <tbody>{children}</tbody>
-          </table>
+        <div>
+            <table className="card table table-stripe">
+                <tbody>{children}</tbody>
+            </table>
         </div>
     )
 }
-export function Heading (props) {
+export function Heading(props) {
     return (
         <h4 className="text-white">
-            <span className= "mr-5">{props.date}</span>
-            <span className= "ml-5">{props.length} Inspections Today</span>
+            <span className="mr-5">{props.date}</span>
+            <span className="ml-5">{props.length} Inspections Today</span>
         </h4>
-   
+
     )
 }
 
@@ -26,13 +26,12 @@ export function Upcoming(props) {
     const history = useHistory();
 
     return (
-        
-                    <tr>
-                        <th scope="row"></th>
-                        <td>{props.date} &nbsp; &nbsp; {props.address} &nbsp; &nbsp; {props.type} &nbsp; &nbsp; {props.permit_id} &nbsp; &nbsp; Notes: props.notes  &nbsp; &nbsp; {props.date_scheduled} &nbsp; {props.admin}</td>
-                        <td className="text-right align-middle"><button className="btn btn-secondary border border-dark" onClick={() => history.push('inspections/' + props.id)}>Results</button>
-                        </td>
-                    </tr>
-               
+        <div className="card-body border border-dark m-1">
+            <tr>
+                <th scope="row"></th>
+                <td>{props.date} &nbsp; &nbsp; {props.address} &nbsp; &nbsp; {props.type} &nbsp; &nbsp; {props.permit_id} &nbsp; &nbsp; Notes: props.notes  &nbsp; &nbsp; {props.date_scheduled} &nbsp; {props.admin}</td>
+                <td><button className="btn btn-secondary border border-dark" onClick={() => history.push('inspections/' + props.id)}>Results</button></td>
+            </tr>
+        </div>
     )
 }
