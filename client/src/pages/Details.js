b136d5523a1c4e1bef387300e571fd8842bfca69
KJ <kj3641402@gmail.com>
2021-07-02 05:28:19 -0400

    made upcoming more responsive.  Got details page showing everything but invoice

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -4,10 +4,10 @@ import { getUser } from '../utils/Session';
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import Nav from "../components/Nav";
-import InspectionInfo from "../components/details/InspectionInfo";
+import {InspectionInfo, NoteDetails} from "../components/details/InspectionInfo";
 
 import InspectionResults from "../components/details/InspectionResults";
-import PermitInfo from "../components/details/InspectionInfo";
+import PermitInfo from "../components/details/PermitInfo";
 import ResultsHistory from "../components/details/ResultsHistory";
 import API from "../utils/API"
 import Moment from 'moment';
@@ -21,6 +21,10 @@ function Details() {
   
     const [inspection, setInspection] = useState([]);
     const [permit, setPermit] = useState([]);
+    const [notes, setNotes] = useState([]);
+    const [results, setResults] = useState([]);
+    const [invoices, setInvoices] = useState([]);
+
     
     const history = useHistory();
     useEffect(() => {
@@ -34,24 +38,29 @@ function Details() {
     async  function loadInspections() {  
        await API.getInspection(id)
        .then(res => {
-         setInspection(res.data)
-         console.log(inspection)
+        setInspection(res.data)
+        setPermit(res.data.Permit)
+        setResults(res.data.results)
+        setNotes(res.data.notes)
+         console.log(res.data)
         }).catch(err => console.log(err));
         
-        await API.getPermit(id)
-       .then(res => {
-         setPermit(res.data)
-            console.log(res.data)
-        }).catch(err => console.log(err));
+    //     await API.getPermit(id)
+    //    .then(res => {
+    //      setPermit(res.data)
+    //      console.log(permit)
+    //     }).catch(err => console.log(err));
     };
-  
+
+    console.log(inspection)
     return (
         <div>
             <Helmet>
                 <title>Details</title>
             </Helmet>
             <Nav />
-            <InspectionInfo key={inspection.id}
+            <InspectionInfo 
+                key={inspection.id}
                 date = {Moment(inspection.date).format("dddd, MMMM Do YYYY")} 
                 address = {inspection.address}
                 type = {inspection.type}
@@ -59,21 +68,39 @@ function Details() {
                 admin = {inspection.admin}
                 date_scheduled = {Moment(inspection.date_scheduled).format("MM- D-YY")}
             />
-            <ResultsHistory/>
+             {notes.map(note => (
+                <NoteDetails 
+                    note = {note.note}
+                    />
+            ))}
+             <div className="container-fluid ml-auto m-3 p-3 border rounded border-primary bg-light" id="previousNotesContainer">
+                    <h3 className="d-flex justify-content-center">Results History</h3>
+                        <table className="table table-bordered">
+                            <tbody>
+            {results.map(result => (
+                <ResultsHistory
+                time = {Moment(result.createdAt).format("lll")}
+                result = {result.result}
+                notes = {result.notes}/>
+            ))}
+                     </tbody>
+                </table>
+
+        </div>
             <InspectionResults/>
             <PermitInfo
-                address = {permit.address}
-                parcel = {permit.parcel_number}
-                owner = {permit.owner}
-                owner_phone = {permit.owner_phone}
-                owner_mobile = {permit.owner_mobile}
-                contractor = {permit.contractor}
-                contractor_phone = {permit.contractor_phone}
-                contractor_email = {permit.contractor_email}
-                id = {permit.id}
-                issued = {permit.issued}
-                expired = {permit.expired}
-                work_description = {permit.work_description}   /> 
+            address = {permit.address}
+            parcel = {permit.parcel_number}
+            owner = {permit.owner}
+            owner_phone = {permit.owner_phone}
+            owner_mobile = {permit.owner_mobile}
+            contractor = {permit.contractor}
+            contractor_phone = {permit.contractor_phone}
+            contractor_email = {permit.contractor_email}
+            id = {permit.id}
+            issued = {permit.issued}
+            expired = {permit.expired}
+            work_description = {permit.work_description}   /> 
            
            
 
