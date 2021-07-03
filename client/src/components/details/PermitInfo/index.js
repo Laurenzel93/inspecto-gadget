00b0f8fe79be17348eb449f64e029eeed7eddfa0
Lauren Wenzel <Laurenzel93@aol.com>
2021-07-03 17:14:01 -0400

    working on formatting of permit info on details

--- client/src/components/details/PermitInfo/index.js
+++ client/src/components/details/PermitInfo/index.js
@@ -3,46 +3,52 @@ import './style.css';
 
 export function PermitInfo(props) {
     return (
-    <div className="" >
-        <div className="container col-10 border rounded border-primary p-4" id="permitInfoContainer">
-            <div className="row justify-content-center">
-                <h3 className="col-12 col-md-10  text-center mb-0 border" >Permit Information</h3>
-   
-            <div style={{ fontWeight: "bold" }} className="col-12 col-md-3  border border-botom-0 m-0 bg-light">Property Information</div>
-            <div className="col-12 col-md-4 border bg-light m-0" id="address"> {props.address}</div>
-            <div className="col-12 col-md-3 border bg-light"id="parcelID">parcel: {props.parcel}</div>
-            <div style={{ fontWeight: "bold" }}  className="col-12 col-md-3 border m-0">Owner Information</div>
-            <div className="col-12 col-md-2 border" id="ownerName">{props.owner}</div>
-            <div className="col-12 col-md-2 border"id="homePhone">ph: {props.owner_phone}</div>
-            <div className="col-12 col-md-3 border"id="cellPhone">cell: {props.owner_mobile}</div>
-            <div style={{ fontWeight: "bold" }}  className="col-12 col-md-3 border  m-0 bg-light">Contractor Information</div>
-            <div className="col-12 col-md-2 border bg-light" id="contractorName">{props.contractor}</div>
-            <div className="col-12 col-md-2 border bg-light"id="contractorPhone">ph: {props.contractor_phone}</div>
-            <div className="col-12 col-md-3 border bg-light"id="contractorEmail">email: {props.contractor_email}</div>
-            <div style={{ fontWeight: "bold" }}  className="col-12 col-md-3 border  m-0">Permit Details</div>
-            <div className="col-12 col-md-2 border" id="permitNumber">{props.id}</div>
-            <div className="col-12 col-md-2 border"id="issuedDate">iss: {props.issued}</div>
-            <div className="col-12 col-md-3 border"id="expirationDate">exp: {props.expired}</div>
-            <div style={{ fontWeight: "bold" }}  className="col-12 col-md-3 border m-0 bg-light">Work Description</div>
-            <div className="col-12 col-md-7 border bg-light" id="workDescription">{props.work_description}</div>
+        <div className="" >
+            <div className="container col-10 border rounded border-primary p-4" id="permitInfoContainer">
+                <div className="row justify-content-center">
+                    <h3 className="col-12 col-md-10  text-center mb-0 border" >Permit Information</h3>
+
+                    <div style={{ fontWeight: "bold" }} className="col-12 col-md-3  border border-botom-0 m-0 bg-light">Property Information</div>
+                    <div className="col-12 col-md-4 border bg-light m-0" id="address"> {props.address}</div>
+                    <div className="col-12 col-md-3 border bg-light" id="parcelID">parcel: {props.parcel}</div>
+                    <div style={{ fontWeight: "bold" }} className="col-12 col-md-3 border m-0">Owner Information</div>
+                    <div className="col-12 col-md-2 border" id="ownerName">{props.owner}</div>
+                    <div className="col-12 col-md-2 border" id="homePhone">ph: {props.owner_phone}</div>
+                    <div className="col-12 col-md-3 border" id="cellPhone">cell: {props.owner_mobile}</div>
+                    <div style={{ fontWeight: "bold" }} className="col-12 col-md-3 border  m-0 bg-light">Contractor Information</div>
+                    <div className="col-12 col-md-2 border bg-light" id="contractorName">{props.contractor}</div>
+                    <div className="col-12 col-md-2 border bg-light" id="contractorPhone">ph: {props.contractor_phone}</div>
+                    <div className="col-12 col-md-3 border bg-light" id="contractorEmail">email: {props.contractor_email}</div>
+                    <div style={{ fontWeight: "bold" }} className="col-12 col-md-3 border  m-0">Permit Details</div>
+                    <div className="col-12 col-md-2 border" id="permitNumber">{props.id}</div>
+                    <div className="col-12 col-md-2 border" id="issuedDate">iss: {props.issued}</div>
+                    <div className="col-12 col-md-3 border" id="expirationDate">exp: {props.expired}</div>
+                    <div style={{ fontWeight: "bold" }} className="col-12 col-md-3 border m-0 bg-light">Work Description</div>
+                    <div className="col-12 col-md-7 border bg-light" id="workDescription">{props.work_description}</div>
+                </div>
             </div>
         </div>
-    </div>    
-     )
+    )
 }
-        
 
-   export  function InvoiceInfo(props) {
+
+export function InvoiceInfo(props) {
+
     return (
-       
-                        <tr>
-                        <td id="item">{props.item}</td>
-                        <td id="quantity">{props.quantity}</td>
-                        <td id="fee">{props.amount_total}</td>
-                        </tr> 
-                  
-   
-        
+
+        <div className="" >
+            <div className="container col-10 border rounded border-primary p-4" id="permitInfoContainer">
+                <div className="row justify-content-center">
+                    <h3 className="col-12 col-md-10  text-center" >Fee Information</h3>
+                    {/* <div style={{ fontWeight: "bold" }} className="col-12 col-md-3  border border-botom-0 m-0 bg-light">Item</div> */}
+                    <div style={{ fontWeight: "bold" }} className="col-12 col-md-3 border bg-light m-0 text-center" id="item"> {props.item}</div>
+                    <div className="col-12 col-md-2 border bg-light text-center" id="quantity">Quantity: {props.quantity}</div>
+                    <div className="col-12 col-md-3 border bg-light text-center" id="cost">${props.amount_total}</div>
+                    <div className="col-12 col-md-3 border bg-light text-right" id="cost">${props.amount_total}</div>
+                </div>
+            </div>
+        </div>
+
 
     )
 }
--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -4,9 +4,9 @@ import { getUser } from '../utils/Session';
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import Nav from "../components/Nav";
-import {InspectionInfo, NoteDetails} from "../components/details/InspectionInfo";
+import { InspectionInfo, NoteDetails } from "../components/details/InspectionInfo";
 import InspectionResults from "../components/details/InspectionResults";
-import { PermitInfo , InvoiceInfo }from "../components/details/PermitInfo";
+import { PermitInfo, InvoiceInfo } from "../components/details/PermitInfo";
 import ResultsHistory from "../components/details/ResultsHistory";
 import API from "../utils/API"
 import Moment from 'moment';
@@ -14,17 +14,17 @@ import Moment from 'moment';
 
 
 function Details() {
-    const {id} = useParams()
-   
+    const { id } = useParams()
+
     // console.log(id)
-  
+
     const [inspection, setInspection] = useState([]);
     const [permit, setPermit] = useState([]);
     const [notes, setNotes] = useState([]);
     const [results, setResults] = useState([]);
     const [invoices, setInvoices] = useState([]);
 
-    
+
     const history = useHistory();
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -35,25 +35,25 @@ function Details() {
         }
     }, [])
 
-    async  function loadInspections() {  
-       await API.getInspection(id)
-       .then ( res  =>  {
-        setInspection(res.data)
-        setPermit(res.data.Permit)
-        setResults(res.data.results)
-        setNotes(res.data.notes)       
-    
-         API.getPermit(res.data.Permit.id)
+    async function loadInspections() {
+        await API.getInspection(id)
             .then(res => {
-                console.log(res.data)
-                setInvoices(res.data.invoices)
-            })
-       }).catch(err => console.log(err));
+                setInspection(res.data)
+                setPermit(res.data.Permit)
+                setResults(res.data.results)
+                setNotes(res.data.notes)
 
-    //    console.log(inspection)
-    //    console.log(permit)
-      
-        
+                API.getPermit(res.data.Permit.id)
+                    .then(res => {
+                        console.log(res.data)
+                        setInvoices(res.data.invoices)
+                    })
+            }).catch(err => console.log(err));
+
+        //    console.log(inspection)
+        //    console.log(permit)
+
+
     }
     return (
         <div>
@@ -61,89 +61,82 @@ function Details() {
                 <title>Details</title>
             </Helmet>
             <Nav />
-            <InspectionInfo 
+            <InspectionInfo
                 key={inspection.id}
-                date = {Moment(inspection.date).format("dddd, MMMM Do YYYY")} 
-                address = {inspection.address}
-                type = {inspection.type}
-                permit_id = {inspection.permit_id}
-                admin = {inspection.admin}
-                date_scheduled = {Moment(inspection.date_scheduled).format("MM- D-YY")}
+                date={Moment(inspection.date).format("dddd, MMMM Do YYYY")}
+                address={inspection.address}
+                type={inspection.type}
+                permit_id={inspection.permit_id}
+                admin={inspection.admin}
+                date_scheduled={Moment(inspection.date_scheduled).format("MM- D-YY")}
             />
-            
+
             {notes.length ? (
                 <div>
-             {notes.map(note => (
-                <NoteDetails 
-                    note = {note.note}
-                    />
-            ))}
-            </div>
-            ) : (
-                <div className= "container mt-0 col-11 mb-2">
-                    <div className="row">
-                        <div className="col- col-md-10  border border-top-0"></div>
-                        </div>    
+                    {notes.map(note => (
+                        <NoteDetails
+                            note={note.note}
+                        />
+                    ))}
                 </div>
-            )}
-             <div className="container-fluid ml-auto m-3 p-3 border rounded border-primary bg-light" id="previousNotesContainer">
-                    <h3 className="d-flex justify-content-center">Results History</h3>
-                        {results.length ? (
-                        <table className="table table-bordered">
-                            <tbody>
-                                {results.map(result => (
-                                    <ResultsHistory
-                                        time = {Moment(result.createdAt).format("lll")}
-                                        result = {result.result}
-                                        notes = {result.notes}/>
-                                ))}
-                            </tbody>
-                        </table>
-                        ) : (
-                            <h6 className="text-center">No Results Submitted</h6>
-                        )}       
+            ) : (
+                    <div className="container mt-0 col-11 mb-2">
+                        <div className="row">
+                            <div className="col- col-md-10  border border-top-0"></div>
+                        </div>
+                    </div>
+                )}
+            <div className="container-fluid ml-auto m-3 p-3 border rounded border-primary bg-light" id="previousNotesContainer">
+                <h3 className="d-flex justify-content-center">Results History</h3>
+                {results.length ? (
+                    <table className="table table-bordered">
+                        <tbody>
+                            {results.map(result => (
+                                <ResultsHistory
+                                    time={Moment(result.createdAt).format("lll")}
+                                    result={result.result}
+                                    notes={result.notes} />
+                            ))}
+                        </tbody>
+                    </table>
+                ) : (
+                        <h6 className="text-center">No Results Submitted</h6>
+                    )}
 
-        </div>
-            <InspectionResults/>
+            </div>
+            <InspectionResults />
             <PermitInfo
-            address = {permit.address}
-            parcel = {permit.parcel_number}
-            owner = {permit.owner}
-            owner_phone = {permit.owner_phone}
-            owner_mobile = {permit.owner_mobile}
-            contractor = {permit.contractor}
-            contractor_phone = {permit.contractor_phone}
-            contractor_email = {permit.contractor_email}
-            id = {permit.id}
-            issued = {Moment(permit.issued).format("l")}
-            expired = {Moment(permit.expired).format("l")}
-            work_description = {permit.description}   /> 
+                address={permit.address}
+                parcel={permit.parcel_number}
+                owner={permit.owner}
+                owner_phone={permit.owner_phone}
+                owner_mobile={permit.owner_mobile}
+                contractor={permit.contractor}
+                contractor_phone={permit.contractor_phone}
+                contractor_email={permit.contractor_email}
+                id={permit.id}
+                issued={Moment(permit.issued).format("l")}
+                expired={Moment(permit.expired).format("l")}
+                work_description={permit.description} />
             {invoices.length ? (
                 <div>
-                     <h3>Fee Information</h3>
-                     <table className="table table-sm table-bordered text-center">
-                        <thead>
-                            <th>Item Name</th>
-                            <th>Quantity</th>
-                            <th>Fee</th>
-                        </thead>
-                            <tbody>
-                                {invoices.map(invoice => (
-                                    <InvoiceInfo
-                                    item = {invoice.item}
-                                    quantity = {invoice.quantity}
-                                    amount_total = {invoice.total}
-                                    />
-                                ))}   
-                            </tbody>
-                     </table>
-                </div>        
-                    
-                   
-                 
-            ) : ( 
-                <h6>There are no Invoiced Items for this Inspection</h6>
-            )}
+                    <div>
+                        {invoices.map(invoice => (
+                            <InvoiceInfo
+                                item={invoice.item}
+                                quantity={invoice.quantity}
+                                amount_total={invoice.total}
+                            />
+                        ))}
+                    </div>
+
+                </div>
+
+
+
+            ) : (
+                    <h6>There are no Invoiced Items for this Inspection</h6>
+                )}
 
 
         </div>
